37f34b0e9c84febbe8beab00e3cbc5f6147b4edd
Matthew MacFarlane <+>
2023-03-05 15:33:39 -0500

    submit objects lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Will Smith",
+    streetAddress: "55 Kubernetes Lane",
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = { ...obj };
+    newObj[key] = value;
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee};
+    newEmployee[key] = undefined;
+    return newEmployee;
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    employee[key] = undefined;
+    return employee;
+}
+
